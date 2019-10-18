96c598360c340ba52618e395d783fa6c80a9f813
Abhigyan <abhigyan709@gmail.com>
2019-10-19 02:27:09 +0530

    "Ninth Commit"

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -21,3 +21,8 @@ urlpatterns += [
     path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
     path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
 ]
+urlpatterns += [
+    path('book/create/', views.BookCreate.as_view(), name='book_create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
+]
