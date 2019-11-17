cd67bcd23292b06e52dacb36af16b0bce04c5e72
Abhigyan <abhigyan709@gmail.com>
2019-11-17 07:23:04 +0530

    "Blog Model Created"

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -147,4 +147,25 @@ class Donate(models.Model):
     phone = models.CharField(max_length=100)
     donator_name = models.CharField(max_length=100, default="Your Name")
     donate_for_free = models.CharField(max_length=5, default="Yes")
-    
+
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
