845eb91a8709ea4cc751e4bd2ac8c04d566dec85
Abhigyan <abhigyan709@gmail.com>
2020-02-17 23:57:39 +0530

    "Reduntant code removed from admin section"

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,30 +1,18 @@
 from django.contrib import admin
-from .models import Author, Book
-from .models import BookInstance
+# from .models import BookInstance
 from .models import Patient, Disease, Doctor, Department
 
+#class BooksInstanceInline(admin.TabularInline):
+ #   model = BookInstance
 
 
-class BooksInline(admin.TabularInline):
-    model = Book
-
-
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-    inlines = [BooksInline]
-
-class BooksInstanceInline(admin.TabularInline):
-    model = BookInstance
-
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
-    inlines = [BooksInstanceInline]
+#@admin.register(Book)
+#class BookAdmin(admin.ModelAdmin):
+   # list_display = ('title', 'author', 'display_genre')
+   # inlines = [BooksInstanceInline]
 
 # register the admin classes for BookInstance using the decorator
-@admin.register(BookInstance)
+""""@admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
     list_display = ('book', 'status', 'borrower', 'due_back', 'id')
@@ -36,9 +24,8 @@ class BookInstanceAdmin(admin.ModelAdmin):
         ('Availability', {
             'fields': ('status', 'due_back', 'borrower')
         }),
-    )
+    )"""
 
-admin.site.register(Author, AuthorAdmin)
 admin.site.register(Patient)
 admin.site.register(Doctor)
 admin.site.register(Disease)
